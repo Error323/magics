f26d7b198e845c2a3b5c31400a97ac8539b05dbf
Error323 <folkerthuizinga@gmail.com>
2013-12-03 17:14:54 +0100

    Remove cuda, add bb and thread

---
+++ src/bitboard.cpp
@@ -0,0 +1,69 @@
+#include "bitboard.h"
+
+void Print3(const U64 a, const U64 b, const U64 c)
+{
+  unsigned char line;
+  for (int i = 7; i >= 0; i--)
+  {
+    printf("%d", i+1);
+
+    line = (a >> (i * 8)) & 0xff;
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\t%d", i+1);
+
+    line = (b >> (i * 8)) & 0xff;
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\t%d", i+1);
+
+    line = (c >> (i * 8)) & 0xff;
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\n");
+  }
+
+  printf("  a b c d e f g h \t  a b c d e f g h \t  a b c d e f g h\n");
+}
+
+void Print2(const U64 a, const U64 b)
+{
+  unsigned char line;
+  for (int i = 7; i >= 0; i--)
+  {
+    printf("%d", i+1);
+
+    line = (a >> (i * 8)) & 0xff;
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\t%d", i+1);
+
+    line = (b >> (i * 8)) & 0xff;
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\n");
+  }
+
+  printf("  a b c d e f g h \t  a b c d e f g h \n");
+}
+
+void Print1(const U64 board)
+{
+  for (int i = 7; i >= 0; i--)
+  {
+    printf("%d", i+1);
+    unsigned char line = (board >> (i * 8)) & 0xff;
+
+    for (int j = 0; j < 8; j++)
+      printf(" %s", ((line >> j) & 1) == 1 ? "1" : ".");
+
+    printf("\n");
+  }
+
+  printf("  a b c d e f g h\n");
+}
---
+++ src/bitboard.h
@@ -0,0 +1,62 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <x86intrin.h>
+
+typedef unsigned long long U64;
+typedef unsigned long U32;
+typedef unsigned short U16;
+typedef unsigned char U8;
+typedef signed long long I64;
+typedef signed long I32;
+typedef signed short I16;
+typedef signed char I8;
+
+#define C64(x) x##ull
+
+inline U64 R64()
+{
+  U64 r = random();
+  r <<= 32;
+  r |= random();
+  return r;
+}
+
+inline U64 R64Few()
+{
+  return R64() & R64() & R64();
+}
+
+/// Pops the least significant bit of the board and returns index
+inline int PopLSB(register U64 &b)
+{
+  int index = __bsfq(b);
+  b &= b - 1;
+  return index;
+}
+
+/// Find the index to the least significant bit
+inline int LSBIndex(register U64 b)
+{
+  return __bsfq(b);
+}
+
+/// Count the number of set bits on a bitboard
+inline int CountBits(register U64 b)
+{
+  return __popcntq(b);
+}
+
+/// Print a bitboard to stdout
+void Print1(const U64 board);
+
+/// Print 2 bitboards to stdout beside eachother
+void Print2(const U64 a, const U64 b);
+
+/// Print 3 bitboards to stdout beside eachother
+void Print3(const U64 a, const U64 b, const U64 c);
+
+#endif // BITBOARD_H
